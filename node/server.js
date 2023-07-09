6d0833cbb71faf96b2d182ed8fa61f9ca25fe43b
Nathalia Lima <nl265080@gmail.com>
2023-07-08 21:28:43 -0300

    Estrutura da aplicação

--- node/server.js
+++ node/server.js
@@ -1,9 +1,62 @@
 import http from 'node:http'
 
+// rotas http = caminhos na api
+
+/* HTTP Status Code: códigos (números de tres digitos) uqe trazem info a respeito de como foi sua requisição, se deu tudo certo, aocnteceu algo etc
+
+*/
+
+/*
+    HTTP
+    - metodo http
+    - URL 
+
+    Metodos mais comuns: 
+    GET: buscar recurso do backend
+    POST: criar recurso
+    PUT: editar /atualizar recurso (atualizando muitos recursos de uma vez)
+    PATCH: atualiar info unica especifica de um recurso (atualizando info especifica)
+    DELETE: deletar um recurso
+
+    o conjunto do metodo http eo recurso (url) é oq da a rota
+    ex:
+    get /users = buscando um usuario no backd=end
+
+*/
+
+// Stateful: aplicação onde a informação sendo guardada em memoria, localmente
+//Stateless: não salva em memoria, salva em coisas externas, como banco de dados 
+
 // para habilitar o uso das importações utilizando o ESModules adicione no arquivo package.json a propriedade "type" com o valor de "module"
 
+//cabeçalhos (requisição/resposta) = metadados
+
+const users = []
+//toda vez que o servidor reinicia a informação vai embora, pois é local
+
 const server = http.createServer((req, res) => { //significa: request e response
-    return res.end('Hello World')
+    const { method, url} = req
+
+    //console.log(method, url)
+
+    if (method == 'GET' && url == '/users'){
+        return res
+        .setHeader('Content-Type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if (method == 'POST' && url == '/users'){
+        users.push({
+            id: 1,
+            name: 'Nath',
+            email: 'nath@exemplo.com'
+        })
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end()
+
+
 })
 
 server.listen(3333)
