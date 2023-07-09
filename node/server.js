9ce2357e1c35267c5fd19d845ff0f3ffa7d3aee7
Nathalia Lima <nl265080@gmail.com>
2023-07-09 20:38:13 -0300

    Streams no Node.js

--- node/server.js
+++ node/server.js
@@ -2,25 +2,24 @@ import http from 'node:http'
 
 // rotas http = caminhos na api
 
-/* HTTP Status Code: códigos (números de tres digitos) uqe trazem info a respeito de como foi sua requisição, se deu tudo certo, aocnteceu algo etc
-
+/* HTTP Status Code: códigos (números de tres digitos) uqe trazem info a respeito de como foi sua requisição, se deu tudo certo, aconteceu algo etc
 */
 
 /*
-    HTTP
+HTTP
     - metodo http
     - URL 
 
-    Metodos mais comuns: 
+Metodos mais comuns: 
     GET: buscar recurso do backend
     POST: criar recurso
     PUT: editar /atualizar recurso (atualizando muitos recursos de uma vez)
     PATCH: atualiar info unica especifica de um recurso (atualizando info especifica)
     DELETE: deletar um recurso
 
-    o conjunto do metodo http eo recurso (url) é oq da a rota
-    ex:
-    get /users = buscando um usuario no backd=end
+o conjunto do metodo http e o recurso (url) é oq da a rota
+exemplo:
+get /users = buscando um usuario no backend
 
 */
 
---
+++ node/streams.js
@@ -0,0 +1,9 @@
+/* 
+obter pequenas partes do recurso antes de ler ele por completo
+
+Readable Streams - stream de leitura
+Writable Streams - stream de escrita - ex netfliz, spotify
+
+no node toda porta de entrada e saida é uma stream
+
+*/
